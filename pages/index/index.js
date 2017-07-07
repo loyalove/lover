8cf50267e846335ab22ba482adcc0c8ee49d7a06
loyalove <598733638@qq.com>
2017-07-07 19:00:30 +0800

    Initial commit

---
+++ pages/index/index.js
@@ -0,0 +1,25 @@
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    motto: '美美&的的',
+    userInfo: {}
+  },
+  //登录事件处理函数
+  doLogin: function (e) {
+    wx.reLaunch({
+      url: '../home/home',
+    })
+  },
+  onLoad: function () {
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function (userInfo) {
+      //更新数据
+      that.setData({
+        userInfo: userInfo
+      })
+    })
+  }
+})
